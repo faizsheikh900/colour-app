75a78111a9c23341994e4b481a168e790e7f97e4
Muhammad Faiz sheikh <159743707+faizsheikh900@users.noreply.github.com>
2024-05-06 05:05:49 +0500

    colour app

---
+++ app.js
@@ -0,0 +1,26 @@
+let result = document.getElementById("GFG");
+ 
+function changeColor(color) {
+    document.body.style.background = color;
+}
+
+function orange() {
+    changeColor('orange');
+    result.innerHTML = "Background Color changed";
+}        
+function yellow() {
+    changeColor('yellow');
+    result.innerHTML = "Background Color changed";
+}        
+function grey() {
+    changeColor('grey');
+    result.innerHTML = "Background Color changed";
+}    
+function red() {
+    changeColor('red');
+    result.innerHTML = "Background Color changed";
+}    
+function green() {
+    changeColor('green');
+    result.innerHTML = "Background Color changed";
+}
